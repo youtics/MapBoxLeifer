9644e81eb3fddd0b30a5b87af8f95cfc85680379
gabriel chaldu <youtics@gmail.com>
2021-11-21 19:29:37 -0300

    version con la carga de las busqueda de inicio y fin + la url con la carga de longitud latitud etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { MapCustomService } from './map-custom.service';
 import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
@@ -8,20 +9,48 @@ import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/co
 })
 export class AppComponent implements OnInit {
 
+  wayPoints:WayPoints = {start:null, end:null};
+  modeInput = 'start';
   @ViewChild('asGeoCoder') asGeoCoder?: ElementRef;
 
-  constructor(private MapCustomService: MapCustomService, private renderer2: Renderer2){}
+  constructor(private mapCustomService: MapCustomService, private renderer2: Renderer2){}
+
   ngOnInit(): void {
-    this.MapCustomService.buildMap()
+    this.mapCustomService.buildMap()
     .then(({geocoder, map})=>{
-
-      //this.asGeoCoder
       this.renderer2.appendChild(this.asGeoCoder?.nativeElement, geocoder.onAdd(map));
-
       console.log('TODO BIEN');
     })
     .catch((error)=>{
       console.log('**************** ERROR **************', error);
     })
+
+    this.mapCustomService.cbAddress.subscribe((getPoint) => {
+      if (this.modeInput == 'start') {
+        this.wayPoints.start = getPoint;
+      }
+      if (this.modeInput == 'end') {
+        this.wayPoints.end = getPoint;
+      }
+    });
   }
+  drawRoute(): void {
+    console.log('***** PUNTOS de ORIGEN y DESTINO', this.wayPoints)
+    const coords = [
+      this.wayPoints.start.center,
+      this.wayPoints.end.center
+    ];
+
+    this.mapCustomService.loadCoords(coords);
+  }
+  changeMode(mode: string): void {
+    this.modeInput = mode;
+  }
+}
+
+export class WayPoints {
+  start: any;
+  end: any
+
+  constructor(){}
 }
--- src/app/map-custom.service.ts
+++ src/app/map-custom.service.ts
@@ -1,7 +1,10 @@
 import { environment } from './../environments/environment';
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import {HttpClient} from "@angular/common/http";
+import {Socket} from "ngx-socket-io";
+
 //import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 @Injectable({
@@ -15,6 +18,10 @@ export class MapCustomService {
   lat = 40.416906;
   long = -3.7056721;
   zoom = 3
+
+  cbAddress: EventEmitter<any> = new EventEmitter<any>();
+  wayPoints: Array<any> = [];
+  markerDriver: any = null;
 
   constructor() { 
     this.mapbox.accessToken = environment.mapPK;
@@ -36,8 +43,22 @@ export class MapCustomService {
          *  TODO: Aqui construimos el input buscador de direcciones
          */
           const geocoder = new MapboxGeocoder({
-            accessToken: mapboxgl.accessToken, mapboxgl:undefined
+            accessToken: mapboxgl.accessToken, 
+            mapboxgl:this.map,
+            marker:false
           });
+
+          geocoder.on('result', ($event) => {
+            const {result} = $event;
+            console.log('*********', result);
+            //limpio la direccion buscada en el imput
+            //geocoder.clear($event);
+
+            /**SI IMPRIMO EL RESULT QUE ME DEVUELVE EL $event ME RETORNA MUCHOS DATOS 
+             * ENTRE ELLOS LAS COORDENADAS - MIRAR LA CONSOLA */
+            
+            this.cbAddress.emit(result);
+          })
   
         resolve({
           map: this.map, geocoder
@@ -48,4 +69,70 @@ export class MapCustomService {
       }
     });
   }
-}
+
+  loadCoords(coords:any): void {
+    console.log("COORDENADAS: ", coords);
+    const url = [
+      `https://api.mapbox.com/directions/v5/mapbox/driving/`,
+      `${coords[0][0]},${coords[0][1]};${coords[1][0]},${coords[1][1]}`,
+      `?steps=true&geometries=geojson&access_token=${environment.mapPK}`,
+    ].join('');
+    console.log(url);
+
+    //this.httpClient.get(url).subscribe((res: any) => {
+/*
+      const data = res.routes[0];
+      const route = data.geometry.coordinates;
+
+      this.map?.addSource('route', {
+        type: 'geojson',
+        data: {
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'LineString',
+            coordinates: route
+          }
+        }
+      });
+
+
+      this.map?.addLayer({
+        id: 'route',
+        type: 'line',
+        source: 'route',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': 'red',
+          'line-width': 5
+        }
+      });
+
+      this.wayPoints = route;
+      this.map?.fitBounds([route[0], route[route.length - 1]], {
+        padding: 100
+      })
+
+      this.socket.emit('find-driver', {points: route});
+
+    });
+*/
+
+  }
+
+ /* addMarkerCustom(coords:any): void {
+    console.log('----->', coords)
+    const el = document.createElement('div');
+    el.className = 'marker';
+    if (!this.markerDriver) {
+      this.markerDriver = new mapboxgl.Marker(el);
+    } else {
+      this.markerDriver
+        .setLngLat(coords)
+        .addTo(this.map);
+    }
+  }*/
+}//fin de la clase
